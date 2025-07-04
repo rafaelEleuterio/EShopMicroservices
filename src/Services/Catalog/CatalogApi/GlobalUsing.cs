a0557dac8d420bc3a523bfba3e2cba271c6cac6a
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2025-07-04 03:14:25 -0300

    Update - GlobalUsing in CatalogApi

--- src/Services/Catalog/CatalogApi/GlobalUsing.cs
+++ src/Services/Catalog/CatalogApi/GlobalUsing.cs
@@ -1,6 +1,7 @@
 ﻿global using Carter;
-global using BuildingBlocks.CQRS;
 global using MediatR;
 global using Mapster;
 global using Marten;
+global using BuildingBlocks.CQRS;
 global using CatalogApi.Models;
+global using CatalogApi.Exceptions;
