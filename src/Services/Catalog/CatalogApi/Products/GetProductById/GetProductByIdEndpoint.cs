3105175c6f150743088e06f27fade5bd1a76bb6a
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2025-07-04 04:44:32 -0300

    Add - GetProductById logic in CatalogApi

---
+++ src/Services/Catalog/CatalogApi/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace CatalogApi.Products.GetProductById;
+
+//public reord GetProductByIdRequest();
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id:guid}",
+            async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+
+                var response = result.Adapt<GetProductByIdResponse>();
+                
+                return Results.Ok(response);
+            })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
+    }
+}
---
+++ src/Services/Catalog/CatalogApi/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CatalogApi.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+internal class GetProductByIQuerydHandler 
+    (IDocumentSession session, ILogger<GetProductByIQuerydHandler> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIQuerydHandler.Handle called with {@Query}", query);
+        
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+        
+        if (product == null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product);
+    }
+}
