e08860c8a2c01f708449fbde72b62d886302c863
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2025-09-05 09:29:22 -0300

    Add - UpdateProduct logic in CatalogApi

---
+++ src/Services/Catalog/CatalogApi/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace CatalogApi.Products.UpdateProduct;
+
+public record UpdateProductCommand(
+    Guid Id,
+    string Name,
+    string Description,
+    string ImageFile,
+    decimal Price,
+    List<string> Category
+) : ICommand<UpdateProductResult>;
+public record UpdateProductResult(bool IsSuccess);
+
+internal class UpdateProductCommandHandler
+    (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+    : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("UpdateProductCommandHandler.Handle called with {@Command}", command);
+        
+        // Load the product from the database
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+        
+        if (product == null)
+        {
+            logger.LogWarning("Product with Id {Id} not found", command.Id);
+            //return new UpdateProductResult(false);
+            throw new ProductNotFoundException();
+        }
+        
+        // Update product properties
+        product.Name = command.Name;
+        product.Description = command.Description;
+        product.ImageFile = command.ImageFile;
+        product.Price = command.Price;
+        product.Category = command.Category;
+        
+        // Save changes to the database
+        session.Update(product);
+        await session.SaveChangesAsync(cancellationToken);
+        
+        return new UpdateProductResult(true);
+    }
+}
