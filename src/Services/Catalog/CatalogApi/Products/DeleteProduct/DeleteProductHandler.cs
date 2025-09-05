f13c82255d371cf434e52cf75a51e4f8fdd79e17
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2025-09-05 09:29:12 -0300

    Add - DeleteProduct logic in CatalogApi

---
+++ src/Services/Catalog/CatalogApi/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace CatalogApi.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+public record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProductCommandHandler
+    (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
+        
+        // Load the product from the database
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+        if (product == null)
+        {
+            logger.LogWarning("Product with Id {Id} not found", command.Id);
+            //return new DeleteProductResult(false);
+
+            throw new ProductNotFoundException();
+        }
+        
+        // Delete the product
+        session.Delete(product);
+        await session.SaveChangesAsync(cancellationToken);
+        
+        return new DeleteProductResult(true);
+    }
+}
