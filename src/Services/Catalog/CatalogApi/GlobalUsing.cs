cea79ca492a23d9d155909a892f10f4f1bfa9d83
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2025-09-06 18:28:12 -0300

    Add Validationto CreateProduct

--- src/Services/Catalog/CatalogApi/GlobalUsing.cs
+++ src/Services/Catalog/CatalogApi/GlobalUsing.cs
@@ -5,3 +5,4 @@ global using Marten;
 global using BuildingBlocks.CQRS;
 global using CatalogApi.Models;
 global using CatalogApi.Exceptions;
+global using FluentValidation;
--- src/Services/Catalog/CatalogApi/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogApi/Products/CreateProduct/CreateProductHandler.cs
@@ -8,8 +8,18 @@ public record CreateProductCommand(string Name,
         : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required!");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required!");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required!");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0!");
+    }
+}
 
-internal class CreateProductCommandHandler(IDocumentSession session)
+internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -17,6 +27,13 @@ internal class CreateProductCommandHandler(IDocumentSession session)
         // Create Product Entity from command object
         // Save the product to the database
         // Return CreateProductResult
+
+        var result = await validator.ValidateAsync(command, cancellationToken);
+        var errors = result.Errors.Select(e => e.ErrorMessage).ToList();
+        if (errors.Any())
+        {
+            throw new ValidationException(errors.FirstOrDefault());
+        }
 
         var produc = new Product
         {
