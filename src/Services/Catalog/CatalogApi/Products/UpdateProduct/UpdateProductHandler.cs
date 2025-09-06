c835ec872bfac2db94942ad52fd554b8ef7e9e0c
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2025-09-06 19:24:23 -0300

    Add Validation to DeleteProduct and UpdateProduct

--- src/Services/Catalog/CatalogApi/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/CatalogApi/Products/UpdateProduct/UpdateProductHandler.cs
@@ -10,6 +10,18 @@ public record UpdateProductCommand(
 ) : ICommand<UpdateProductResult>;
 public record UpdateProductResult(bool IsSuccess);
 
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required!");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required!").Length(2, 150).WithMessage("Name must be between 2 and 150 characters!");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required!");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required!");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0!");
+    }
+}
+
 internal class UpdateProductCommandHandler
     (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
     : ICommandHandler<UpdateProductCommand, UpdateProductResult>
