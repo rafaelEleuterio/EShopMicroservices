101c9153782245f55b65d78e7c0965105ed04c62
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2025-09-08 15:50:27 -0300

    Add pagination to GetProducts

--- src/Services/Catalog/CatalogApi/GlobalUsing.cs
+++ src/Services/Catalog/CatalogApi/GlobalUsing.cs
@@ -9,3 +9,4 @@ global using FluentValidation;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
 global using CatalogApi.Data;
+global using Marten.Pagination;
